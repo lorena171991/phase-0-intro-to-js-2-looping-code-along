676877ae5e8c7eab6e43f1108c303d8f102c7e34
Lorena Aragon <lorena171991@yahoo.com>
2023-03-28 00:47:57 -0700

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Code your solutions in this file
+function countDown() {
+    let countDown = 0;
+    while (countDown < 11) {
+        console.log(countDown++);
+    }
+}
+countDown(11);
+
+function writeCards(names, event) {
+    for (let i = 0; i < names.length; i++) {
+      
+      names[i] = `Thank you, ${names[i]}, for the wonderful ${event} gift!`;
+      
+    }
+    return(names); 
+  }
